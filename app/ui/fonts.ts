fb86a721b63c4dc6bd1579c714b9be8502a47a02
LinPan-asbuilt <lin.pan@asbuiltdigital.com>
2024-09-22 12:29:39 +1200

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Sofadi_One, Lusitana } from "next/font/google";
+
+export const font = Sofadi_One({ subsets: ["latin"], weight: "400" });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ['400', '700'], });
