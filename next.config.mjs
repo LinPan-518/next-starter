40c4d81b83b6519a99de310a33fbad4c3d3221af
LinPan <wennypan518@gmail.com>
2024-09-22 12:56:35 +1200

    completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
+ 
+const nextConfig = {
+    experimental: {
+      ppr: 'incremental',//“增量”值允许您对特定路由采用 PPR。
+    },
+  };
+   
+  export default nextConfig;
